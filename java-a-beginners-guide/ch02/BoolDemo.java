1bba635be3753ce8912e1de15a6d6983e3e49592
Rickard Körkkö <rickard@rickardk.se>
2022-11-05 15:23:41 +0100

    book(9781260463552): ch02 - page 39-44

---
+++ java-a-beginners-guide/ch02/BoolDemo.java
@@ -0,0 +1,27 @@
+/*
+    Demonstrate boolean values
+ */
+
+class BoolDemo {
+    public static void main(String[] args) {
+        boolean b;
+
+        b = false;
+        System.out.println("b is " + b);
+
+        b = true;
+        System.out.println("b is " + b);
+
+        if(b) {
+            System.out.println("this is executed");
+        }
+
+        b = false;
+
+        if(b) {
+            System.out.println("this is not executed");
+        }
+
+        System.out.println("10 > 9 is " + (10 > 9)); // parentheses required as + has higher precedence than the >
+    }
+}
---
+++ java-a-beginners-guide/ch02/StrDemo.java
@@ -0,0 +1,9 @@
+// Demonstrate the use of strings and escape sequences
+
+class StrDemo {
+    public static void main(String[] args) {
+        System.out.println("First line\nSecond line");
+        System.out.println("A\tB\tC");
+        System.out.println("C\tD\tE");
+    }
+}

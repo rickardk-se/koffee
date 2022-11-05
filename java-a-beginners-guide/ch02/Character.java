ff81efddd59be651fe770eaaafb1ea86470b6286
Rickard Körkkö <rickard@rickardk.se>
2022-11-05 11:15:12 +0100

    book(9781260463552): ch02 - page 33-38

---
+++ java-a-beginners-guide/ch02/Character.java
@@ -0,0 +1,17 @@
+class Character {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+
+        System.out.println("ch contains " + ch);
+
+        ch++;
+
+        System.out.println("ch contains " + ch);
+
+        ch = 90;
+
+        System.out.println("ch contains " + ch);
+    }
+}

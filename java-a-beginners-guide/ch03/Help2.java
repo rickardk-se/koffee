f332fb0899066e6e78a00ff5699768e47752a5f3
Rickard Körkkö <rickard@rickardk.se>
2022-11-09 21:49:58 +0100

    book(9781260463552): ch03 - page 87-104

--- java-a-beginners-guide/ch03/Help2.java
+++ java-a-beginners-guide/ch03/Help2.java
@@ -1,13 +1,21 @@
 class Help2 {
     public static void main(String[] args) throws java.io.IOException {
-        System.out.println("Help on:");
-        System.out.println("  1. if");
-        System.out.println("  2. switch");
-        System.out.print("Choose one: ");
 
-        char choice;
+        char choice, ignore;
 
-        choice = (char) System.in.read();
+        do {
+            System.out.println("Help on:");
+            System.out.println("  1. if");
+            System.out.println("  2. switch");
+            System.out.println("  3. for");
+            System.out.println("  4. while");
+            System.out.println("  5. do-while");
+            System.out.print("Choose one: ");
+            choice = (char) System.in.read();
+            do {
+                ignore = (char) System.in.read();
+            } while(ignore != '\n');
+        } while(choice < '1' | choice > '5');
 
         switch(choice) {
             case '1':
@@ -24,8 +32,21 @@ class Help2 {
                 System.out.println("  // ...");
                 System.out.println("}");
                 break;
-            default:
-                System.out.println("Invalid choice");
+            case '3':
+                System.out.println("The for:\n");
+                System.out.print("for(init; condition; iteration)");
+                System.out.println(" statement;");
+                break;
+            case '4':
+                System.out.println("The while:\n");
+                System.out.println("while(condition) statement;");
+                break;
+            case '5':
+                System.out.println("The do-while:\n");
+                System.out.println("do {");
+                System.out.println("    statement;");
+                System.out.println("} while(condition);");
+                break;
         }
     }
 }
